d03e9f05282480a0dc1e096dd36b3a18bbd46785
GeorgeJuniorBoadu <george.boadu@amalitech.com>
2024-01-31 11:20:54 +0000

    feat: FizzBuzz terminal up setup

---
+++ src/main/java/org/javaupskill/Main.java
@@ -0,0 +1,37 @@
+package org.javaupskill;
+
+import java.util.InputMismatchException;
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Lets Play FizzBuzz Game");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Enter your number");
+
+            int playerNumber = scanner.nextInt();
+                System.out.println("The Fizz, Buzz and FizzBuzz numbers are: \n");
+            while (playerNumber >= 0) {
+
+                if (playerNumber % 3 == 0) {
+                    System.out.println("Fizz \t");
+
+                } else if (playerNumber % 5 == 0) {
+                    System.out.println("Buzz \t");
+
+                } else if (playerNumber % 3 == 0 && playerNumber % 5 == 0) {
+
+                    System.out.println("FizzBuzz \t");
+
+                } else {
+                    System.out.println(playerNumber);
+                }
+                playerNumber-=1;
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Please enter an integer");
+        }
+    }
+}
